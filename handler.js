0f52ab823dfeb9ae8b755e7aa2c281e53a95b82d
ccarlostuy <121572108+ccarlostuy@users.noreply.github.com>
2022-12-29 18:59:19 -0600

    Update handler.js
    calcular promedio

--- handler.js
+++ handler.js
@@ -1,45 +1,70 @@
-'use strict';
-
-module.exports.helloworld = async (event,context,callback) => {
-  const response = {
-    statusCode: 200,
-    condicion=3
-    body: JSON.stringify(
-      {
-               
-        message: "printHelloWorld()",
-        input: event,
-    
-    //programa que calcula el factoria de un número entero
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "https://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <title>Promedio de 3 notas</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script>
  
-var numero = prompt('Ingrese un número entero positivo:'); //ingreso 4 por ejemplo
- 
-for(i=1; i<=numero-1; i++){
-var resultado=numero;
-resultado = resultado * i
+function algoritmo()
+{
+    let nota_1, nota_2, nota_3, promedio;
+    nota_1 = parseFloat (document.formulario1.nota_1.value);
+    nota_2 = parseFloat (document.formulario1.nota_2.value);
+    nota_3 = parseFloat (document.formulario1.nota_3.value);
+    promedio=(nota_1+nota_2+nota_3)/3;
+    if(promedio<7)
+        alert('Reporbado');
+    else
+        alert('Aprobado');
+    document.formulario1.promedio.value = promedio;
 }
-document.write(resultado);
-// me devuelve como resultad 12
-      },
-      null,
-      2
-    ),
-  };
-callback(null,response)
-};
-
-function GCD(a,b){
- var result;
  
- if(a % b > 0){
-  result = GCD( b, a % b );
- }else{
-  result = b;
- }
- 
- return result;
-}
-function printHelloWorld(){
-  return "Hello World";
-}
-module.exports.printHelloWorld=printHelloWorld;
+        </script>
+    </head>
+    <body>
+        <form name="formulario1">
+            <table style="text-align: left; margin-left: auto; margin-right: auto;">
+                <tbody>
+                    <tr>
+                        <td>
+                            <label for="nota_1">Ingresa el valor de nota 1:</label>
+                        </td>
+                        <td>
+                            <input name="nota_1" required="required" step="0.000001" type="number" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="nota_2">Ingresa el valor de nota 2:</label>
+                        </td>
+                        <td>
+                            <input name="nota_2" required="required" step="0.000001" type="number" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="nota_3">Ingresa el valor de nota 3:</label>
+                        </td>
+                        <td>
+                            <input name="nota_3" required="required" step="0.000001" type="number" />
+                        </td>
+                    </tr>
+                    <tr align="center">
+                        <td colspan="2" rowspan="1">
+                            <input value="Procesar" type="button" onclick="algoritmo();" />
+                            <input type="reset" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label for="promedio">Valor de promedio:</label>
+                        </td>
+                        <td>
+                            <input name="promedio" step="0.000001" type="number" />
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </body>
+</html>
